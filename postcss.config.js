acb7ad94a89c57487ceea6124ee3449f3ebe91f8
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-09-15 09:53:18 +0100

    :wrench: Create postcss config file.

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+  ]
+}
