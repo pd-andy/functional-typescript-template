8cebecc178f2df744d91991d1e43d195a7115b70
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-10-12 15:22:59 +0100

    :wrench: General updates to config files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
-  "extends": [
-    "plugin:@typescript-eslint/recommended",
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'typescript': true
+  },
+  'extends': [
+    'plugin:@typescript-eslint/recommended', // Out of the box Typescript rules
+    'standard' // Out of the box StandardJS rules
   ],
-  "plugins": ["@typescript-eslint/eslint-plugin"],
-  "env": {
-    "browser": true,
-  },
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "typescript": true
-  },
-  "rules": {
-    "@typescript-eslint/explicit-function-return-type": [2, {
-      "allowExpressions": true,
-      "allowTypedFunctionExpressions": true,
-      "allowHigherOrderFunctions": true,
+  'plugins': [
+    '@typescript-eslint' // Let's us override rules below.
+  ],
+  'rules': {
+    '@typescript-eslint/no-use-before-define': 'off', // Allows us to hoist variables and functions which I am a fan of, functions not variables that is.
+    '@typescript-eslint/no-explicit-any': 'off', // Too strict for my case, sometimes I need an any type
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', { // Prevents us from using any delimiter for interface properties.
+      'multiline': {
+        'delimiter': 'none',
+        'requireLast': false
+      },
+      'singleline': {
+        'delimiter': 'comma',
+        'requireLast': false
+      }
     }],
-    "@typescript-eslint/no-unused-vars": [2, {
-      "argsIgnorePattern": "_"
-    }]
+    '@typescript-eslint/indent': 'off', // This is the job of StandardJS, they are competing rules so we turn off the Typescript one. 
+    '@typescript-eslint/no-unused-vars': ['error', {
+      'argsIgnorePattern': '_'
+    }],
+    'no-undef': 'off', // see: https://github.com/typescript-eslint/typescript-eslint/issues/342
+    'node/no-unsupported-features/es-syntax': 'off' // Allows us to use Import and Export keywords.
   }
 }
